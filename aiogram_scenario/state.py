3a77bef93b696b7234d6a7a068238770227d4d17
Abstract-X <abstract-x-mail@protonmail.com>
2020-06-15 16:40:44 +1100

    Restructure state

--- aiogram_scenario/state.py
+++ aiogram_scenario/state.py
@@ -1,4 +1,3 @@
-from __future__ import annotations
 from abc import ABC, abstractmethod
 import logging
 
@@ -8,10 +7,81 @@ from aiogram import Dispatcher
 logger = logging.getLogger(__name__)
 
 
-def _log_registration_handler(state, callback, handler_type):
+def _log_registration_handler(state_name, callback, handler_type):
 
-    logger.debug(f"For the state '{state}', a {handler_type}_handler "
+    logger.debug(f"For the state '{state_name}', a {handler_type}_handler "
                  f"is registered: '{callback.__name__}'")
+
+
+class HandlersRegistrar:
+    """ Handlers registrar, used to register handlers for a specific target_state. """
+
+    def __init__(self, dispatcher: Dispatcher, state_name: str):
+
+        self._dispatcher = dispatcher
+        self._state_name = state_name
+
+    def register_message_handler(self, callback, *custom_filters, commands=None, regexp=None, content_types=None,
+                                 run_task=None, **kwargs) -> None:
+
+        self._dispatcher.register_message_handler(callback, *custom_filters, commands=commands, regexp=regexp,
+                                                  content_types=content_types, state=self._state_name,
+                                                  run_task=run_task, **kwargs)
+        _log_registration_handler(self._state_name, callback, "message")
+
+    def register_callback_query_handler(self, callback, *custom_filters, run_task=None, **kwargs) -> None:
+
+        self._dispatcher.register_callback_query_handler(callback, *custom_filters, state=self._state_name,
+                                                         run_task=run_task, **kwargs)
+        _log_registration_handler(self._state_name, callback, "callback_query")
+
+    def register_channel_post_handler(self, callback, *custom_filters, commands=None, regexp=None,
+                                      content_types=None, run_task=None, **kwargs) -> None:
+
+        self._dispatcher.register_channel_post_handler(callback, *custom_filters, commands=commands, regexp=regexp,
+                                                       content_types=content_types, state=self._state_name,
+                                                       run_task=run_task, **kwargs)
+        _log_registration_handler(self._state_name, callback, "channel_post")
+
+    def register_chosen_inline_handler(self, callback, *custom_filters, run_task=None, **kwargs) -> None:
+
+        self._dispatcher.register_chosen_inline_handler(callback, *custom_filters, state=self._state_name,
+                                                        run_task=run_task, **kwargs)
+        _log_registration_handler(self._state_name, callback, "chosen_inline")
+
+    def register_edited_channel_post_handler(self, callback, *custom_filters, commands=None, regexp=None,
+                                             content_types=None, run_task=None, **kwargs) -> None:
+
+        self._dispatcher.register_edited_channel_post_handler(callback, *custom_filters, commands=commands,
+                                                              regexp=regexp, content_types=content_types,
+                                                              state=self._state_name, run_task=run_task, **kwargs)
+        _log_registration_handler(self._state_name, callback, "edited_channel_post")
+
+    def register_edited_message_handler(self, callback, *custom_filters, commands=None, regexp=None,
+                                        content_types=None, run_task=None, **kwargs) -> None:
+
+        self._dispatcher.register_edited_message_handler(callback, *custom_filters, commands=commands, regexp=regexp,
+                                                         content_types=content_types, state=self._state_name,
+                                                         run_task=run_task, **kwargs)
+        _log_registration_handler(self._state_name, callback, "edited_message")
+
+    def register_inline_handler(self, callback, *custom_filters, run_task=None, **kwargs) -> None:
+
+        self._dispatcher.register_inline_handler(callback, *custom_filters, state=self._state_name,
+                                                 run_task=run_task, **kwargs)
+        _log_registration_handler(self._state_name, callback, "inline")
+
+    def register_pre_checkout_query_handler(self, callback, *custom_filters, run_task=None, **kwargs) -> None:
+
+        self._dispatcher.register_pre_checkout_query_handler(callback, *custom_filters, state=self._state_name,
+                                                             run_task=run_task, **kwargs)
+        _log_registration_handler(self._state_name, callback, "pre_checkout_query")
+
+    def register_shipping_query_handler(self, callback, *custom_filters, run_task=None, **kwargs) -> None:
+
+        self._dispatcher.register_shipping_query_handler(callback, *custom_filters, state=self._state_name,
+                                                         run_task=run_task, **kwargs)
+        _log_registration_handler(self._state_name, callback, "shipping_query")
 
 
 class AbstractState(ABC):
@@ -26,12 +96,13 @@ class AbstractState(ABC):
 
     def __init__(self):
 
-        self.raw_value = self.__class__.__name__
+        self.name = self.__class__.__name__
 
     def __str__(self):
 
-        return self.__class__.__name__
+        return self.name
 
+    @abstractmethod
     async def process_transition(self, *args, **kwargs) -> None:
         """ Performs target_state transition logic.
             It has all the same arguments as the handler.
@@ -44,65 +115,3 @@ class AbstractState(ABC):
         """ Registers target_state handlers. """
 
         pass
-
-
-class HandlersRegistrar:
-    """ Handlers registrar, used to register handlers for a specific target_state. """
-
-    def __init__(self, dispatcher: Dispatcher, state: AbstractState):
-
-        self._dispatcher = dispatcher
-        self._state = state
-
-    def register_message_handler(self, callback, *custom_filters, commands=None, regexp=None, content_types=None,
-                                 run_task=None, **kwargs) -> None:
-        self._dispatcher.register_message_handler(callback, *custom_filters, commands=commands, regexp=regexp,
-                                                  content_types=content_types, state=self._state.raw_value,
-                                                  run_task=run_task, **kwargs)
-        _log_registration_handler(self._state, callback, "message")
-
-    def register_callback_query_handler(self, callback, *custom_filters, run_task=None, **kwargs) -> None:
-        self._dispatcher.register_callback_query_handler(callback, *custom_filters, state=self._state.raw_value,
-                                                         run_task=run_task, **kwargs)
-        _log_registration_handler(self._state, callback, "callback_query")
-
-    def register_channel_post_handler(self, callback, *custom_filters, commands=None, regexp=None,
-                                      content_types=None, run_task=None, **kwargs) -> None:
-        self._dispatcher.register_channel_post_handler(callback, *custom_filters, commands=commands, regexp=regexp,
-                                                       content_types=content_types, state=self._state.raw_value,
-                                                       run_task=run_task, **kwargs)
-        _log_registration_handler(self._state, callback, "channel_post")
-
-    def register_chosen_inline_handler(self, callback, *custom_filters, run_task=None, **kwargs) -> None:
-        self._dispatcher.register_chosen_inline_handler(callback, *custom_filters, state=self._state.raw_value,
-                                                        run_task=run_task, **kwargs)
-        _log_registration_handler(self._state, callback, "chosen_inline")
-
-    def register_edited_channel_post_handler(self, callback, *custom_filters, commands=None, regexp=None,
-                                             content_types=None, run_task=None, **kwargs) -> None:
-        self._dispatcher.register_edited_channel_post_handler(callback, *custom_filters, commands=commands,
-                                                              regexp=regexp, content_types=content_types,
-                                                              state=self._state.raw_value, run_task=run_task, **kwargs)
-        _log_registration_handler(self._state, callback, "edited_channel_post")
-
-    def register_edited_message_handler(self, callback, *custom_filters, commands=None, regexp=None,
-                                        content_types=None, run_task=None, **kwargs) -> None:
-        self._dispatcher.register_edited_message_handler(callback, *custom_filters, commands=commands, regexp=regexp,
-                                                         content_types=content_types, state=self._state.raw_value,
-                                                         run_task=run_task, **kwargs)
-        _log_registration_handler(self._state, callback, "edited_message")
-
-    def register_inline_handler(self, callback, *custom_filters, run_task=None, **kwargs) -> None:
-        self._dispatcher.register_inline_handler(callback, *custom_filters, state=self._state.raw_value,
-                                                 run_task=run_task, **kwargs)
-        _log_registration_handler(self._state, callback, "inline")
-
-    def register_pre_checkout_query_handler(self, callback, *custom_filters, run_task=None, **kwargs) -> None:
-        self._dispatcher.register_pre_checkout_query_handler(callback, *custom_filters, state=self._state.raw_value,
-                                                             run_task=run_task, **kwargs)
-        _log_registration_handler(self._state, callback, "pre_checkout_query")
-
-    def register_shipping_query_handler(self, callback, *custom_filters, run_task=None, **kwargs) -> None:
-        self._dispatcher.register_shipping_query_handler(callback, *custom_filters, state=self._state.raw_value,
-                                                         run_task=run_task, **kwargs)
-        _log_registration_handler(self._state, callback, "shipping_query")
