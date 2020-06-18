1e35356d5cf4f05c298f797908f1fd909830b00b
Abstract-X <abstract-x-mail@protonmail.com>
2020-06-18 18:38:51 +1100

    Add registrars imports

--- aiogram_scenario/__init__.py
+++ aiogram_scenario/__init__.py
@@ -2,6 +2,8 @@ from .state import AbstractState
 from .states_map import StatesMap
 from .middleware import FSMMiddleware
 from .fsm import FSM, FSMPointer
+from .registrars.state import StateRegistrar
+from .registrars.common import CommonRegistrar
 
 
 __version__ = "0.1.0"
