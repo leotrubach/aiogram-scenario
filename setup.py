20887c452aedbd150e7bb06a0d15dbadca012031
Abstract-X <abstract-x-mail@protonmail.com>
2020-07-29 16:10:46 +1100

    Change way getting version

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
 import setuptools
+from pathlib import Path
+import re
 
-import aiogram_scenario
+
+LIB_NAME = "aiogram-scenario"
+PACKAGE_NAME = "aiogram_scenario"
+WORK_DIR = Path(__file__).parent
+
+
+def fetch_version() -> str:
+
+    content = (WORK_DIR / PACKAGE_NAME / "__init__.py").read_text(encoding="UTF-8")
+    try:
+        version_string = re.findall(r'__version__ = "\d+\.\d+\.\d+"', content)[0]
+        version = version_string.rsplit(" ")[-1].replace('"', "")
+        return version
+    except IndexError:
+        raise RuntimeError("version not found!")
 
 
 setuptools.setup(
-    name="aiogram-scenario",
-    version=aiogram_scenario.__version__,
-    packages=setuptools.find_packages(),
+    name=LIB_NAME,
+    version=fetch_version(),
+    packages=setuptools.find_packages(exclude=("docs",)),
     url="https://github.com/Abstract-X/aiogram-scenario",
     license="MIT",
     author="Abstract-X",
