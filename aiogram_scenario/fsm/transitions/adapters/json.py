dd17193b4877adfe95e81689285aa5d1d6ca30f6
Abstract-X <abstract-x-mail@protonmail.com>
2021-02-18 01:56:23 +1100

    Add support for optional direction key in JSON transitions adapter

--- aiogram_scenario/fsm/transitions/adapters/json.py
+++ aiogram_scenario/fsm/transitions/adapters/json.py
@@ -6,6 +6,21 @@ from .base import AbstractTransitionsAdapter
 
 class JSONTransitionsAdapter(AbstractTransitionsAdapter):
 
+    def __init__(self, *, optional_direction: str = ""):
+
+        self._optional_direction = optional_direction
+
     def parse_transitions(self, content: str) -> Dict[str, Dict[str, str]]:
 
-        return json.loads(content)
+        raw_transitions = json.loads(content)
+        transitions = {}
+
+        for source_state in raw_transitions:
+            transitions[source_state] = {}
+            for handler in raw_transitions[source_state]:
+                transitions[source_state][handler] = {}
+                for raw_direction, destination_state in raw_transitions[source_state][handler].items():
+                    direction = None if raw_direction == self._optional_direction else raw_direction
+                    transitions[source_state][handler][direction] = destination_state
+
+        return transitions
