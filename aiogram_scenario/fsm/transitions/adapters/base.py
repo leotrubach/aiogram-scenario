462d8e337d98c7e558875caf1cd469266c150d54
Abstract-X <abstract-x-mail@protonmail.com>
2021-03-28 17:59:53 +1100

    Fix transitions adapters; add TOML adapter

--- aiogram_scenario/fsm/transitions/adapters/base.py
+++ aiogram_scenario/fsm/transitions/adapters/base.py
@@ -1,8 +1,11 @@
 from abc import ABC, abstractmethod
-from typing import Optional
+from typing import Optional, Dict, Union
 import re
 
 from aiogram_scenario.fsm.types import RawTransitionsType
+
+
+RawBaseTransitionsType = Dict[str, Dict[str, Union[str, Dict[str, str]]]]
 
 
 def _get_new_name(name: str, prefix: Optional[str], postfix: Optional[str]) -> str:
@@ -29,9 +32,9 @@ def _check_handler_name(name: str) -> None:
         raise ValueError(f"incorrect handler name '{name}'!")
 
 
-def _check_direction_name(name: str) -> None:
+def _check_direction_name(name: Optional[str]) -> None:
 
-    if (name is not None) and re.fullmatch(r"\S+", name) is None:
+    if (name is not None) and (re.fullmatch(r"\S+", name) is None):
         raise ValueError(f"incorrect direction name '{name}'!")
 
 
@@ -41,9 +44,13 @@ def _check_naming(transitions: RawTransitionsType) -> None:
         _check_state_name(source_state)
         for handler in transitions[source_state]:
             _check_handler_name(handler)
-            for direction in transitions[source_state][handler]:
-                _check_direction_name(direction)
-                _check_state_name(transitions[source_state][handler][direction])
+            element = transitions[source_state][handler]
+            if isinstance(element, str):
+                _check_state_name(element)
+            elif isinstance(element, dict):
+                for direction, destination_state in element.items():
+                    _check_direction_name(direction)
+                    _check_state_name(destination_state)
 
 
 def _get_augmented_transitions(transitions: RawTransitionsType, handlers_prefix: Optional[str],
@@ -66,20 +73,24 @@ def _get_augmented_transitions(transitions: RawTransitionsType, handlers_prefix:
     return augmented_transitions
 
 
+def _get_transitions_with_optional_directions(transitions: RawBaseTransitionsType) -> RawTransitionsType:
+
+    transitions_ = {}
+    for source_state in transitions:
+        transitions_[source_state] = {}
+        for handler in transitions[source_state]:
+            transitions_[source_state][handler] = {}
+            element = transitions[source_state][handler]
+            if isinstance(element, str):
+                transitions_[source_state][handler][None] = element
+            elif isinstance(element, dict):
+                for direction, destination_state in element.items():
+                    transitions_[source_state][handler][direction] = destination_state
+
+    return transitions_
+
+
 class AbstractTransitionsAdapter(ABC):
-
-    # noinspection PyMethodMayBeStatic
-    def _fetch(self, filename: str, encoding: str) -> str:
-
-        with open(filename, encoding=encoding) as file_wrapper:
-            content = file_wrapper.read()
-
-        return content
-
-    @abstractmethod
-    def _parse(self, content: str) -> RawTransitionsType:
-
-        pass
 
     def load(self, filename: str, encoding: str = "UTF-8", *,
              handlers_prefix: Optional[str] = None,
@@ -91,8 +102,23 @@ class AbstractTransitionsAdapter(ABC):
         transitions = self._parse(content)
 
         _check_naming(transitions)
+        transitions = _get_transitions_with_optional_directions(transitions)
+
         if any((handlers_prefix, handlers_postfix, states_prefix, states_postfix)):
             transitions = _get_augmented_transitions(transitions, handlers_prefix, handlers_postfix,
                                                      states_prefix, states_postfix)
 
         return transitions
+
+    # noinspection PyMethodMayBeStatic
+    def _fetch(self, filename: str, encoding: str) -> str:
+
+        with open(filename, encoding=encoding) as file_wrapper:
+            content = file_wrapper.read()
+
+        return content
+
+    @abstractmethod
+    def _parse(self, content: str) -> RawBaseTransitionsType:
+
+        pass
--- aiogram_scenario/fsm/transitions/adapters/json.py
+++ aiogram_scenario/fsm/transitions/adapters/json.py
@@ -1,25 +1,10 @@
 import json
 
-from .base import AbstractTransitionsAdapter
-from aiogram_scenario.fsm.types import RawTransitionsType
+from .base import AbstractTransitionsAdapter, RawBaseTransitionsType
 
 
 class JSONTransitionsAdapter(AbstractTransitionsAdapter):
 
-    def _parse(self, content: str) -> RawTransitionsType:
+    def _parse(self, content: str) -> RawBaseTransitionsType:
 
-        raw_transitions = json.loads(content)
-        transitions = {}
-
-        for source_state in raw_transitions:
-            transitions[source_state] = {}
-            for handler in raw_transitions[source_state]:
-                transitions[source_state][handler] = {}
-                element = raw_transitions[source_state][handler]
-                if isinstance(element, str):
-                    transitions[source_state][handler][None] = element
-                elif isinstance(element, dict):
-                    for direction, destination_state in element.items():
-                        transitions[source_state][handler][direction] = destination_state
-
-        return transitions
+        return json.loads(content)
